4184705560d559a98badef97bf59b357129e75e9
abouelkheir <mohamed.abouelkheir41@gmail.com>
2024-04-25 08:15:23 +0200

    adding some svg for service box widget

--- lib/main.dart
+++ lib/main.dart
@@ -30,11 +30,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const Home(),
     );
   }
 }
-
